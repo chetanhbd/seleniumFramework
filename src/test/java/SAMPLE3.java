b3e37770a5c5d0b149c336116c2f1ad261d836a4
chetanhbd <chetanhbd@gmail.com>
2022-04-08 14:04:41 +0530

    NEWCODE

--- src/test/java/SAMPLE3.java
+++ src/test/java/SAMPLE3.java
@@ -1,2 +1,17 @@
 public class SAMPLE3 {
+    //static -  to access the static-  object is not required.
+    //datatype: int, char, long, float, boolean etc.....
+    //Syntax(rule): <accessmodifier> <returntype> <functionname>
+
+    public static int addition (int a, int b){ //a and b are parameters
+        // a and b have the scope inside addition function only(access modifier-private)
+        return a+b;
+    }
+
+    public static double addition(double a, double b){
+
+        return a+b;
+    }
+
+
 }
---
+++ src/test/java/sample3_1.java
@@ -0,0 +1,11 @@
+public class sample3_1 {
+
+    public static void main(String[] args){
+
+
+        System.out.println(SAMPLE3.addition(1,2));
+
+        System.out.println(SAMPLE3.addition(4.5,3.5));
+
+    }
+}
